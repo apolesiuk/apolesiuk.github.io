23d2c824ffe18cd9fa1f62a9d5a0151bbd2963b2
apolesiuk <olesiukanastasiya@gmail.com>
2024-11-10 13:30:38 +0300

    Add files via upload

---
+++ math/stepform/forchange.js
@@ -0,0 +1,66 @@
+function verify() {
+    console.log("a")
+    let a = parseInt(elementA.value);
+    console.log(a)
+    let x =0, y=0, z=0, q=0;
+
+    if (a >= 500) {
+        x=Math.floor(a / 500)
+        a=a%500
+    }
+    if (a>=100) {
+        y=Math.floor(a / 100)
+        a=a%100
+    }
+    if (a>=10) {
+        z=Math.floor(a / 10)
+        a=a%10
+    }
+    if (a>=2) {
+        q=Math.floor(a / 2)
+        a=a%2
+    }
+
+    if (a!=0){
+        result = 'разменять невозможно'
+        document.getElementById("result").innerText = messageText + result;
+        document.getElementsByName('result')[0].value = result;
+        check = false;
+    }
+    else{
+        result = x + ' купюр по 500 \n' + y + ' купюр по 100 \n' + z + ' купюр по 10 \n' + q + ' монет по 2 \n' 
+        document.getElementById("result").innerText = messageText + result;
+        document.getElementsByName('result')[0].value = result;
+        check = true;
+    }
+}
+
+function send() {
+    if (check) {
+        let textCondition = document.getElementsByTagName('p')[0].innerText
+        document.getElementsByName('formulation')[0].value = textCondition;
+        document.getElementsByName('result')[0].value = result;
+        document.getElementById("UserEnter").submit();
+    } else {
+        alert("Есть недостатки. Повторите ввод")
+    }
+}
+
+function verify_send() {
+    verify();
+    send();
+}
+
+let messageText = document.getElementById("result").innerText
+console.log(messageText)
+let result;
+let check = false;
+
+const elementA = document.getElementById("a");
+elementA.addEventListener('input', verify);
+
+const elementVerify = document.getElementById("verify");
+elementVerify.addEventListener('click', verify);
+
+const elementSend = document.getElementById("send");
+elementSend.addEventListener('click', send)
