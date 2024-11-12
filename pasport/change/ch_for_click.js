5b8019cf57c9e49200755d493a958700b5e17358
apolesiuk <olesiukanastasiya@gmail.com>
2024-11-12 18:26:08 +0300

    Add files via upload

---
+++ pasport/change/ch_for_click.js
@@ -0,0 +1,17 @@
+const node_for_click = document.getElementById("for_click")
+
+function find_edit(){
+    const item_f  = document.getElementById('fam')
+    console.log(item_f.innerText)
+    item_f.innerHTML="<b>ALESIYUK</b>"
+
+    const item_n  = document.getElementById('name')
+    console.log(item_n.innerText)
+    item_n.innerHTML="<b>ANASTASIYA</b>"
+
+    const item_dt  = document.getElementById('otch')
+    console.log(item_dt.innerText)
+    item_dt.innerHTML="<b>PAVLOVNA</b>"
+}
+
+node_for_click.addEventListener("click",find_edit)
